9f04e050059b3ec58ee7e559598fdcc193c80f5c
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-06-13 13:38:26 +0200

    add tanstack

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import App from './Core/App';
 import reportWebVitals from './reportWebVitals';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const queryClient = new QueryClient();
 root.render(
+  <QueryClientProvider client={queryClient}>
     <App />
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function

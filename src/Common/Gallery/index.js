c33a831c92b4e48cdf2b3c5f5f066764bff086b9
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-06-14 22:51:00 +0200

    add pagination

--- src/Common/Gallery/index.js
+++ src/Common/Gallery/index.js
@@ -1,16 +1,41 @@
 import { useQuery } from "@tanstack/react-query";
 import { GalleryBox, StyledGallery } from "./styled";
 import { PokemonTile } from "../PokemonTile";
+import { fetchGallery } from "../../Core/API";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export const Gallery = () => {
-  const fetchGallery = async () => (
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=24&offset=0").then(
-      (response) => response.json()
-    )
-  )
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [offset, setOffset] = useState(0); // początkowa wartość offset
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const offsetValue = searchParams.get('offset');
+    if (offsetValue) {
+      setOffset(Number(offsetValue));
+    }
+  }, [location.search]);
+
+  const handleNextPage = () => {
+    const newOffset = offset + 100;
+    setOffset(newOffset);
+    navigate(`/pokemon?offset=${newOffset}`);
+  };
+
+  const handlePrevPage = () => {
+    const newOffset = offset - 100;
+    setOffset(newOffset);
+    navigate(`/pokemon?offset=${newOffset}`);
+  };
+
+  const limit = 100
+
+
   const { isLoading, isError, data } = useQuery(
-    ["gallery"],
-    fetchGallery
+    ["gallery", { limit: limit, offset: offset }],
+    () => fetchGallery(limit, offset)
   );
 
   if (isLoading) return (<div>Loading</div>)
@@ -18,13 +43,21 @@ export const Gallery = () => {
 
   return data ? (
     <GalleryBox>
+      <button disabled={offset === 0} onClick={handlePrevPage}>
+        Prev page
+      </button>
+      <button onClick={handleNextPage}>
+        Next page
+      </button>
       <StyledGallery>
         {data.results.map((item, index) => (<PokemonTile
           key={index}
           name={item.name}
-          id={index}
+          id={index + offset}
         />))}
       </StyledGallery>
+
     </GalleryBox>
+
   ) : null
 }
--- src/Core/API.tsx
+++ src/Core/API.tsx
@@ -1,1 +1,8 @@
-export const imageAPI = (id: number) => {`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${id}.svg`}
+//export const imageAPI = (id: number) => { `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${id}.svg` }
+
+export const fetchGallery = async (limit: number, offset: number): Promise<any> => {
+  const params = new URLSearchParams({ limit: String(limit), offset: String(offset) })
+  return fetch(`https://pokeapi.co/api/v2/pokemon?${params}`).then(
+    (response) => response.json()
+  )
+};

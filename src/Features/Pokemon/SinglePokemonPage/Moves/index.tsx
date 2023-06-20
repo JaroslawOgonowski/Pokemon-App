6e7c9772df68d4f338ae66402282b9b77cf85796
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-06-21 00:58:01 +0200

    add 1st gen moves

--- src/Features/Pokemon/SinglePokemonPage/Moves/index.tsx
+++ src/Features/Pokemon/SinglePokemonPage/Moves/index.tsx
@@ -28,17 +28,33 @@ export interface MovesProps {
   moves: MoveEntry[] | undefined;
 }
 
+export const Moves = ({ moves }: MovesProps) => {
+  console.log(moves);
+  const filteredMoves = moves?.filter((move) =>
+    move.version_group_details.some(
+      (detail) => detail.version_group.name === "red-blue"
+    )
+  );
 
-export const Moves = ({moves}: MovesProps) => {
-  console.log(moves)
- return (
+  return (
     <>
       <h2>Moves</h2>
-
-      ⬅<div>Generartion I</div>➡
-  
-
-
+      ⬅<div>Generation I</div>➡
+      <div>
+        {filteredMoves?.map((move) => {
+          const capitalizedMoveName =
+            move.move.name.charAt(0).toUpperCase() +
+            move.move.name.slice(1);
+          return (
+            <div key={move.move.name}>
+              {capitalizedMoveName}
+              <div>Level Learned At: {move.version_group_details[0].level_learned_at}</div>
+              <div>Move Learn Method: {move.version_group_details[0].move_learn_method.name}</div>
+              <div>Move Learn Method URL: {move.version_group_details[0].move_learn_method.url}</div>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
+  );
 };

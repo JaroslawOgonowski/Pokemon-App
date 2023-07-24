b52e487927171d4a65e4424f0bb94f82fe42656f
Jarosław Ogonowski <dza.ogonowski@gmail.com>
2023-07-24 08:05:54 +0200

    restyled types page

--- src/Features/Types/TypesList/styled.js
+++ src/Features/Types/TypesList/styled.js
@@ -7,7 +7,15 @@ export const StyledTypesList = styled.div`
   justify-content: center;
   align-items: center;
   gap: 2vh;
+  margin-top: -14vh;
+  padding-top: 12vh;
   padding-bottom: 10vh;
+  background-image: linear-gradient(
+    0deg,
+    rgba(40, 36, 36, 0.963) 19%,
+    rgba(0, 0, 0, 0.97) 74%,
+    rgba(0, 0, 0, 1) 100%
+  );
 `;
 
 export const TypesP = styled.p`
@@ -21,4 +29,5 @@ export const TypesP = styled.p`
 export const TypesRealationsImg = styled.img`
   width: 98%;
   height: 100%;
+  padding-top: 8vh;
 `;
